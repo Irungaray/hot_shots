bb790d1fda86165eaae459e2373661e27bedeae7
Lionel Irungaray <lionel.irungaray@aicore.com.ar>
2022-06-02 21:13:14 -0300

    Add shell scripts

---
+++ set_analog_output.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pacmd set-default-sink "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink"
+~
---
+++ set_bt_output.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ pacmd set-default-sink "bluez_sink.93_AC_89_5B_22_F2.a2dp_sink"
+
